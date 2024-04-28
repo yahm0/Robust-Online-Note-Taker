b02fe8330839108d081fb59da102ec71d3336032
yahm0 <stnt190@outlook.com>
2024-04-28 10:36:13 -0400

    added logic to the server.js file for the html and api routes

--- server.js
+++ server.js
@@ -4,3 +4,32 @@ const fs = require('fs');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('Public'));
+
+// HTML Routes
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/Public/index.html'));
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/Public/notes.html'));
+
+// API Routes
+app.get('/api/notes', (req, res) =>
+    fs.readFile(path.join(__dirname, '/DB/database.json'), (err, data) => {
+        if (err) throw err;
+        res.json(JSON.parse(data));
+    })
+);
+
+app.post('/api/notes', (req, res) => {
+    fs.readFile(path.join(__dirname, '/DB/database.json'), (err, data) => {
+        if (err) throw err;
+        const notes = JSON.parse(data);
+        const newNote = req.body;
+        notes.push(newNote);
+        fs.writeFile(path.join(__dirname, '/DB/database.json'), JSON.stringify(notes), (err) => {
+            if (err) throw err;
+            res.json(newNote);
+        });
+    });
+}
